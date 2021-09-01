8d9b9f725a2bec3579fe6f56206f1e0d150df557
TaniaGuillen <89371496+TaniaGuillen@users.noreply.github.com>
2021-09-01 15:37:51 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,6 +1,15 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    a = int(input('Dame el año: '))
+
+ if a%4=0:
+     print('es año bisiesto')
+ elif a%100=0:
+     print('No es año bisiesto')
+ else:
+     print('No es año bisiesto')
+        
     pass
 
 if __name__=='__main__':
