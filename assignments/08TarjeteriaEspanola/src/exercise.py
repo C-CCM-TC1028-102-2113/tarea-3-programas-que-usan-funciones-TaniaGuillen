a713ef321c91f875cede3866af57af883ae883a2
TaniaGuillen <89371496+TaniaGuillen@users.noreply.github.com>
2021-09-01 15:20:54 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,18 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    papel = int(input('dame el numero de pliegos que tienes: '))
+plumon = int(input('dame el numero de plumones que tienes: '))
+
+print('Numero de tarjetas maximas posibles')
+pp = (papel*12)
+np = (plumon*35)
+
+if np <pp:
+    print(np)
+if pp < np:
+    print(pp)
+    
     pass
 
 if __name__=='__main__':
